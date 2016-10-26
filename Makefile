da6314a3109cf094dbea6a03c76b09f595728e38
Rodrigo Sepúlveda <rodrigo.sepulveda.12@sansano.usm.cl>
2016-10-26 20:22:55 -0300

    seed añadido al makefile

--- Makefile
+++ Makefile
@@ -7,12 +7,14 @@ SRCDIR = src
 IDDIR = Instancias
 BINDIR = bin
 KDIR_P = mkdir -p
+
 i=1
+seed = 1000
 
 comp:$(PROG)
 
 run:$(PROG)
-	$(BINDIR)/$(PROG) Instancia$(i).rwsp
+	$(BINDIR)/$(PROG) Instancia$(i).rwsp $(seed)
 
 $(PROG):
 	$(KDIR_P) $(BINDIR)
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
+/* Variables globales */
+char* archivo_instancia;
+int seed;
+
+
 int main(int argc, char const *argv[])
 {
-	cout << "hola mundo" << endl;
+	cout << "\n\n--------------------------------------------------------------" << endl;
+	cout << "Problema:\t\tRotating Workforce Scheduling Problem" << endl;
+	cout << "Heurísticas usadas:\tGreedy, Tabu Search" << endl;
+	cout << "Autor:\t\t\tRodrigo Sepúlveda Garrido" << endl;
+	cout << "--------------------------------------------------------------" << endl;
+
+	if (argc <= 2)
+	{
+		cerr << "\033[1;31mERROR: Faltan parámetros\033[0m" << endl;
+		return -1;
+	}
+	archivo_instancia = (char*)argv[1];
+	seed = atoi(argv[2]);
+
+	cout << archivo_instancia << endl;
+	cout << seed << endl;
+
+
 	return 0;
 }
